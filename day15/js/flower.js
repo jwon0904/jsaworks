0cd1397a8acb68bc283bbfd506618f0919c2bbb4
jwon0904 <jwon0904@naver.com>
2022-05-27 18:17:39 +0900

    day15 js

---
+++ day15/js/flower.js
@@ -0,0 +1,10 @@
+function showText(){
+    let show = document.getElementById("item2")
+    show.style.display="block";
+    document.getElementById("item3").style.display="none";
+}
+function hideText(){
+    let hide = document.getElementById("item2")
+    hide.style.display="none"
+    document.getElementById("item3").style.display="block";
+}
